3c69d9f579fd101252574555bd1986a341baa9a9
jonas <jonas.voisard@gmail.com>
2023-08-07 03:03:29 +0200

    Add page placeholder

--- src/routes/[eventId]/edit/pages/+page.svelte
+++ src/routes/[eventId]/edit/pages/+page.svelte
@@ -1,3 +1,4 @@
-<div class="w-[210mm]">
-  prout
+<div class="w-[210mm] bg-base-200 card grid place-content-center mt-2">
+  <span class="opacity-50">Sélectione une page pour l'éditer</span>
+  
 </div>
