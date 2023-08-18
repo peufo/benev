3cd8e1192ad9a3d2f3278568f8773a6c013d2ed7
jonas <jonas.voisard@gmail.com>
2023-08-18 12:42:51 +0200

    fix timezone offset

--- src/lib/material/input/InputDatetime.svelte
+++ src/lib/material/input/InputDatetime.svelte
@@ -1,19 +1,21 @@
 <script lang="ts">
 	import FormControl from './FormControl.svelte'
-	import type {InputProps} from '.'
+	import type { InputProps } from '.'
 	type $$Props = InputProps
 	$: ({ input, value: _value, ...props } = $$props as $$Props)
 	export let value = _value
 </script>
 
 <FormControl {...props} prefix="date" let:key>
+	{#if value}
+		<input type="hidden" name={key} value={new Date(value).toJSON()} />
+	{/if}
 	<input
 		bind:value
 		on:input
 		on:focus
 		on:blur
 		type="datetime-local"
-		name={key}
 		id={key}
 		class="input-bordered input"
 		{...input}
