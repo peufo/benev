ee839a37b8e24eaded84d7923f963245367ebb77
jonas <jonas.voisard@gmail.com>
2023-08-21 21:45:30 +0200

    show timezoneoffset

---
+++ src/routes/root/+page.server.ts
@@ -0,0 +1,3 @@
+export const load = async () => ({
+	timezoneOffsetServer: new Date().getTimezoneOffset(),
+})
--- src/routes/root/+page.svelte
+++ src/routes/root/+page.svelte
@@ -1,5 +1,18 @@
-<h1>HELLO ROOT</h1>
+<script lang="ts">
+	export let data
+</script>
 
-<a href="/root/mails" class="link link-hover">
-  Model de mail
-</a>
+<div class="flex flex-col gap-4">
+	<h1>HELLO ROOT</h1>
+
+	<a href="/root/mails" class="link link-hover"> Model de mail </a>
+
+	<div>
+		<p>
+			timezone client: <b>{new Date().getTimezoneOffset()}</b>
+		</p>
+		<p>
+			timezone server: <b>{data.timezoneOffsetServer}</b>
+		</p>
+	</div>
+</div>
