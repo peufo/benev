3429b3d7834dc779cdabbb08cb9003017983805f
jonas <jonas.voisard@gmail.com>
2023-08-30 10:56:21 +0200

    Add teams link

--- src/routes/[eventId]/invite/+page.svelte
+++ src/routes/[eventId]/invite/+page.svelte
@@ -11,7 +11,10 @@
 	<Card class="max-w-2xl mx-auto">
 		<span slot="title">Super! Tu es membre de "{data.event.name}"</span>
 		<br />
-		<a href={$eventPath} class="btn"> Aller à la page d'accueil</a>
+		<div class="grid grid-cols-2 gap-2">
+			<a href={$eventPath} class="btn"> Vers la page d'accueil</a>
+			<a href="{$eventPath}/teams" class="btn"> Voir les secteurs</a>
+		</div>
 	</Card>
 {:else}
 	<MemberForm event={data.event} userId={data.user.id} class="mx-auto" />
