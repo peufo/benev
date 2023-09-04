bade6af5bc77f59b4659a6f98b2800afbf2277a6
jonas <jonas.voisard@gmail.com>
2023-09-04 20:04:52 +0200

    fix sort period

--- src/lib/plan/PeriodCard.svelte
+++ src/lib/plan/PeriodCard.svelte
@@ -5,18 +5,18 @@
 
 	export let period: Period & { subscribes: Subscribe[] }
 	export let origin: dayjs.Dayjs
-	export let hourHeight: number // houre
+	export let hourHeight: number
 
 	$: msHeight = hourHeight / (1000 * 60 * 60)
 	$: top = -origin.diff(period.start) * msHeight
 	$: height = dayjs(period.end).diff(period.start) * msHeight
-	$: console.log(height)
 </script>
 
 <div
 	class="
 		absolute left-2 right-0
 		bg-base-200 border rounded-md p-2 text-sm
+		hover:z-10
 	"
 	style:top="{top}px"
 	style:height="{height}px"
--- src/lib/plan/Plan.svelte
+++ src/lib/plan/Plan.svelte
@@ -8,7 +8,7 @@
 
 	export let teams: (Team & { periods: (Period & { subscribes: Subscribe[] })[] })[]
 
-	let rowHeight = 40
+	let hourHeight = 40
 
 	const periods = teams
 		.map(({ periods }) => periods.map((p) => [p.start, p.end]))
@@ -16,9 +16,11 @@
 		.sort()
 
 	const range = {
-		start: dayjs(periods[0]).startOf('day'),
-		end: dayjs(periods.at(-1)).endOf('day'),
+		start: dayjs(periods.at(-1)).startOf('day'),
+		end: dayjs(periods[0]).endOf('day'),
 	}
+
+	console.log({ range })
 
 	const days: Dayjs[] = []
 	for (let day = range.start; day.isBefore(range.end); day = day.add(1, 'day')) {
@@ -27,23 +29,22 @@
 </script>
 
 <Card>
-	<input type="number" class="input" bind:value={rowHeight} />
+	<input type="number" class="input" bind:value={hourHeight} />
 
 	<div class="max-h-[615px] overflow-auto table-pin-cols snap-x scroll-pl-16 bordered">
 		<div class="flex min-w-max pr-2">
 			<div class="sticky left-0 z-20 bg-base-100">
 				{#each days as day}
 					<div class="w-16">
-						<div class="sticky top-0 bg-base-100 pr-2 border-b" style:height="{rowHeight}px">
+						<div class="sticky top-0 bg-base-100 pr-2 border-b" style:height="{hourHeight}px">
 							<div class="text-sm font-medium">{day.format('ddd D')}</div>
 							<div class="text-xs">{day.format('MMMM')}</div>
 						</div>
-
 						<div>
 							{#each Array(23).fill(0) as h, index}
 								<div
 									class="flex items-center bg-base-100 text-center font-light"
-									style:height="{rowHeight}px"
+									style:height="{hourHeight}px"
 								>
 									<span class="grow pl-4 pr-1 text-sm">
 										{(index + 1).toString().padStart(2, '0')}
@@ -62,7 +63,7 @@
 
 			{#each teams as team}
 				<div class="snap-start pl-2 relative">
-					<div class="w-52 sticky top-0 pb-2 z-10" style:height="{rowHeight}px">
+					<div class="w-52 sticky top-0 pb-2 z-10" style:height="{hourHeight}px">
 						<div class="flex items-center rounded bg-base-200 h-full px-2">
 							<span class="overflow-hidden whitespace-nowrap text-ellipsis font-medium">
 								{team.name}
@@ -71,7 +72,7 @@
 					</div>
 
 					{#each team.periods as period}
-						<PeriodCard {period} origin={range.start} hourHeight={rowHeight} />
+						<PeriodCard {period} origin={range.start} {hourHeight} />
 					{/each}
 				</div>
 			{/each}
