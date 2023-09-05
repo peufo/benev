101ac1ddafa6092aa980fdc736f1dd27de4e9e8d
jonas <jonas.voisard@gmail.com>
2023-09-05 20:35:11 +0200

    fix z-index

--- src/lib/plan/Plan.svelte
+++ src/lib/plan/Plan.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-	import { Card } from '$lib/material'
-	import { Period, Subscribe, Team } from '@prisma/client'
 	import dayjs, { type Dayjs } from 'dayjs'
 	import 'dayjs/locale/fr-ch'
+	import { Card } from '$lib/material'
+	import { Period, Subscribe, Team } from '@prisma/client'
 	import PeriodCard from './PeriodCard.svelte'
+	import WorkInProgress from '$lib/WorkInProgress.svelte'
 	import { eventPath } from '$lib/store'
 	dayjs.locale('fr-ch')
 
@@ -26,37 +27,43 @@
 	for (let day = range.start; day.isBefore(range.end); day = day.add(1, 'day')) {
 		days.push(day)
 	}
+
+	const hours = Array(24)
+		.fill(0)
+		.map((v, index) => (index + 1).toString().padStart(2, '0'))
 </script>
 
 <Card>
-	<input type="number" class="input" bind:value={hourHeight} />
+	<div class="flex">
+		<WorkInProgress />
+		<input type="number" class="input" bind:value={hourHeight} />
+	</div>
 
-	<div
-		bind:offsetWidth={containerWidth}
-		class="max-h-[615px] overflow-auto table-pin-cols snap-x scroll-pl-16 bordered"
-	>
-		<div class="flex min-w-max pr-2" style="--container-width: {containerWidth}px;">
+	<div class="max-h-[615px] overflow-auto table-pin-cols snap-x scroll-pl-16 bordered">
+		<div
+			class="flex min-w-max pr-2 z-10"
+			style="--container-width: {containerWidth}px;"
+			bind:offsetWidth={containerWidth}
+		>
 			<div class="sticky left-0 z-20 bg-base-100">
 				{#each days as day}
 					<div class="w-16">
-						<div class="sticky top-0 bg-base-100 pr-2 border-b z-10" style:height="{hourHeight}px">
+						<div
+							class="sticky top-0 bg-base-100 pr-2 border-b border-r z-10"
+							style:height="{hourHeight}px"
+						>
 							<div class="text-sm font-medium">{day.format('ddd D')}</div>
 							<div class="text-xs">{day.format('MMMM')}</div>
 						</div>
 						<div>
-							{#each Array(23).fill(0) as h, index}
+							{#each hours.slice(0, 23) as hour}
 								<div
-									class="flex items-center bg-base-100 text-center font-light scale"
+									class="flex items-center bg-base-100 text-center font-light border-r"
 									style:height="{hourHeight}px"
 								>
-									<span class="grow pl-4 pr-1 text-sm">
-										{(index + 1).toString().padStart(2, '0')}
+									<span class="grow text-sm">
+										{hour}
 									</span>
-									<div class="w-4 border-b border-r h-full flex flex-col justify-evenly items-end">
-										<div class="border-b w-1/3" />
-										<div class="border-b w-1/2" />
-										<div class="border-b w-1/3" />
-									</div>
 								</div>
 							{/each}
 						</div>
@@ -64,11 +71,21 @@
 				{/each}
 			</div>
 
+			<div class="w-0">
+				{#each days as day}
+					{#each hours as hour}
+						<div class="scale" style:height="{hourHeight}px" />
+					{/each}
+				{/each}
+			</div>
+
 			{#each teams as team}
-				<div class="snap-start pl-2 relative z-20">
+				<div class="snap-start pl-2 relative">
 					<div class="w-36 sticky top-0 pb-2 z-10" style:height="{hourHeight}px">
-						<div class="relative flex items-center rounded bg-base-200 h-full px-2 outline outline-base-100">
-							<a href="{$eventPath}/teams/{team.id}" class="absolute inset-0">{" "}</a>
+						<div
+							class="relative flex items-center rounded bg-base-200 h-full px-2 outline outline-base-100"
+						>
+							<a href="{$eventPath}/teams/{team.id}" class="absolute inset-0">{' '}</a>
 							<span class="overflow-hidden whitespace-nowrap text-ellipsis font-medium">
 								{team.name}
 							</span>
@@ -93,12 +110,6 @@
 		position: absolute;
 		height: 100%;
 		width: calc(var(--container-width) - 64px);
-		left: 100%;
 		border-bottom-width: 1px;
-	}
-	.scale:first-child::after {
-		top: -1px;
-		height: calc(100% + 1px);
-		border-top-width: 1px;
 	}
 </style>
