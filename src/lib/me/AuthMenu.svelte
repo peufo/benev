95987499069eafdbc818ab97c736d72e9a5a830c
jonas <jonas.voisard@gmail.com>
2023-10-10 17:57:16 +0200

    place all admin routes in menu

--- src/lib/me/AuthMenu.svelte
+++ src/lib/me/AuthMenu.svelte
@@ -19,7 +19,7 @@
 </script>
 
 {#if userName}
-	<DropDown tippyProps={{trigger: 'mouseenter mouseleave click'}}>
+	<DropDown tippyProps={{trigger: 'focus mouseenter mouseleave click'}}>
 		<button slot="activator" class="btn btn-square btn-ghost lg:inline-flex lg:w-auto lg:px-2">
 			<Icon path={mdiAccountOutline} />
 			<span class="hidden lg:block">{userName || ''}</span>
--- src/routes/[eventId]/EventMenu.svelte
+++ src/routes/[eventId]/EventMenu.svelte
@@ -3,85 +3,115 @@
 	import { eventPath } from '$lib/store'
 	import { page } from '$app/stores'
 	import { Icon } from '$lib/material'
-	import { mdiCogOutline, mdiMenu } from '@mdi/js'
+	import {
+		mdiFileDocumentOutline,
+		mdiHomeOutline,
+		mdiMapMarkerRadiusOutline,
+		mdiMenu,
+		mdiApplicationCogOutline,
+		mdiChartGantt,
+		mdiFileDocumentMultipleOutline,
+		mdiAccountMultipleOutline,
+		mdiClipboardTextMultipleOutline,
+		mdiCogs,
+	} from '@mdi/js'
+	import DropDown from '$lib/material/DropDown.svelte'
 	export let pageIndex: LayoutData['pageIndex']
 	export let pages: LayoutData['pages']
 	export let isOwner: boolean
 	export let isLeaderInEvent: boolean
+
+	const adminPages = [
+		{
+			path: '/admin/manage/members',
+			label: 'Membres',
+			icon: mdiAccountMultipleOutline,
+		},
+		{
+			path: '/admin/manage/subscribes',
+			label: 'Inscriptions',
+			icon: mdiClipboardTextMultipleOutline,
+		},
+		{
+			path: '/admin/plan',
+			label: 'Planification',
+			icon: mdiChartGantt,
+
+		},
+		{
+			path: '/admin/config',
+			label: 'Configuration',
+			icon: mdiCogs,
+
+		},
+		{
+			path: '/admin/pages',
+			label: 'Les pages',
+			icon: mdiFileDocumentMultipleOutline,
+
+		},
+	]
+
+
 </script>
 
-<ul class="tabs hidden lg:block">
-	<a href={$eventPath} class="tab tab-bordered" class:tab-active={$page.route.id === '/[eventId]'}>
-		{pageIndex.title}
-	</a>
-	<a
-		href="{$eventPath}/teams"
-		class="tab tab-bordered"
-		class:tab-active={$page.route.id?.startsWith('/[eventId]/teams')}
-	>
-		Secteurs
-	</a>
-
-	{#each pages as { title, path, id, isIndex } (id)}
-		{@const href = `${$eventPath}${isIndex ? '' : `/${path}`}`}
-		<a class="tab tab-bordered" {href} class:tab-active={$page.url.pathname == href}>
-			{title}
-		</a>
-	{/each}
-</ul>
-
-{#if isOwner || isLeaderInEvent}
-	<a
-		href="{$eventPath}/admin"
-		class="
-			hidden lg:btn btn-square
-			{$page.route.id?.startsWith(`/[eventId]/admin`) ? '' : 'lg:btn-ghost'}
-		"
-	>
-		<Icon path={mdiCogOutline} title="Gestion évènement" class="rotate-12" />
-	</a>
-{/if}
-
-<div class="dropdown dropdown-bottom dropdown-end lg:hidden">
-	<!-- svelte-ignore a11y-label-has-associated-control -->
-	<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-	<label tabindex="0" class="btn btn-ghost btn-square">
+<DropDown class="max-h-none" tippyProps={{ trigger: 'focus mouseenter mouseleave click' }}>
+	<button slot="activator" class="btn btn-ghost btn-square">
 		<Icon path={mdiMenu} />
-	</label>
-	<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-	<ul
-		tabindex="0"
-		class="dropdown-content z-10 menu menu-lg p-2 shadow-lg bg-base-100 rounded-box w-64"
-	>
-		{#if isOwner || isLeaderInEvent}
-			{@const active = $page.url.pathname.startsWith(`${$eventPath}/admin`)}
-			<li>
-				<a href="{$eventPath}/admin" class:active>
-					<Icon path={mdiCogOutline} class="rotate-12" {active} />
-					Gestion évènement
-				</a>
-			</li>
-		{/if}
+	</button>
 
-		<li>
-			<a href={$eventPath} class:active={$page.route.id == '/[eventId]'}>
-				{pageIndex.title}
-			</a>
-		</li>
+	<div class="flex flex-col gap-1">
+		<!-- HOME -->
+		<a href={$eventPath} class="menu-item" class:active={$page.route.id == '/[eventId]'}>
+			<Icon
+				path={mdiHomeOutline}
+				size={22}
+				class="opacity-80"
+				active={$page.route.id == '/[eventId]'}
+			/>
+			{pageIndex.title}
+		</a>
 
-		<li>
-			<a href="{$eventPath}/teams" class:active={$page.route.id?.startsWith('/[eventId]/teams')}>
-				Secteurs
-			</a>
-		</li>
+		<!-- TEAMS -->
+		<a
+			href="{$eventPath}/teams"
+			class="menu-item"
+			class:active={$page.route.id?.startsWith('/[eventId]/teams')}
+		>
+			<Icon
+				path={mdiMapMarkerRadiusOutline}
+				size={22}
+				class="opacity-80"
+				active={$page.route.id?.startsWith('/[eventId]/teams')}
+			/>
+			Secteurs
+		</a>
 
+		<!-- PAGES -->
 		{#each pages as { title, path, id, isIndex } (id)}
 			{@const href = `${$eventPath}${isIndex ? '' : `/${path}`}`}
-			<li>
-				<a {href} class:active={$page.url.pathname == href}>
-					{title}
-				</a>
-			</li>
+			<a {href} class="menu-item" class:active={$page.url.pathname == href}>
+				<Icon
+					path={mdiFileDocumentOutline}
+					size={22}
+					class="opacity-80"
+					active={$page.url.pathname == href}
+				/>
+				{title}
+			</a>
 		{/each}
-	</ul>
-</div>
+
+		<hr>
+
+		<!-- ADMIN -->
+		{#if isOwner || isLeaderInEvent}
+			{#each adminPages as {path, label, icon}}
+				{@const active = $page.route.id?.startsWith(`/[eventId]${path}`)}
+				<a href="{$eventPath}{path}" class="menu-item" class:active>
+					<Icon path={icon} size={22} class="opacity-80" {active} />
+					{label}
+				</a>
+			{/each}
+		{/if}
+	</div>
+</DropDown>
