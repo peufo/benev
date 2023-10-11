c3f775dce9ae39d7b5770054c22ed82ec0456236
jonas <jonas.voisard@gmail.com>
2023-10-11 08:33:19 +0200

    remove useless tab

--- src/routes/[eventId]/admin/+layout.svelte
+++ src/routes/[eventId]/admin/+layout.svelte
@@ -1,61 +1,11 @@
 <script lang="ts">
-	import {
-		mdiHomeOutline,
-		mdiApplicationCogOutline,
-		mdiChartGantt,
-		mdiFileDocumentMultipleOutline,
-	} from '@mdi/js'
-	import { page } from '$app/stores'
-	import { eventPath } from '$lib/store'
-	import { Icon } from '$lib/material'
-
-	const tabs = [
-		{
-			path: '/admin',
-			label: 'Gestion',
-			icon: mdiHomeOutline,
-			reg: /\/admin(\/manage.*)?$/,
-		},
-		{
-			path: '/admin/plan',
-			label: 'Planification',
-			icon: mdiChartGantt,
-			reg: /\/admin\/plan/,
-		},
-		{
-			path: '/admin/config',
-			label: 'Configuration',
-			icon: mdiApplicationCogOutline,
-			reg: /\/admin\/config/,
-		},
-		{
-			path: '/admin/pages',
-			label: 'Les pages',
-			icon: mdiFileDocumentMultipleOutline,
-			reg: /\/admin\/pages.*/,
-		},
-	]
+	export let data
 </script>
 
 <svelte:head>
-	<title>BENEV - Administration</title>
+	<title>Gestion - {data.event.name}</title>
 </svelte:head>
 
 <div class="flex flex-col gap-4 max-w-4xl m-auto">
-	<div class="tabs tabs-boxed bg-base-100 grid grid-cols-1 md:grid-cols-4 shadow-lg">
-		{#each tabs as { path, label, reg, icon }}
-			{@const href = `${$eventPath}${path}`}
-			{@const active = !!$page.url.pathname.match(reg)}
-			<a
-				class="tab tab-lg flex-nowrap grow justify-start md:justify-center"
-				{href}
-				class:tab-active={active}
-			>
-				<Icon path={icon} class="mr-3 opacity-70" size={22} {active} />
-				<span>{label}</span>
-			</a>
-		{/each}
-	</div>
-
 	<slot />
 </div>
