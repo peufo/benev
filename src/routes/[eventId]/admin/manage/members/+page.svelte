391235d2b114bd978ab5e95d60a07721b4f4bd80
jonas <jonas.voisard@gmail.com>
2023-10-11 14:45:59 +0200

    place select columns with other control

--- src/routes/[eventId]/admin/manage/members/+page.svelte
+++ src/routes/[eventId]/admin/manage/members/+page.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
+	import { mdiSigma } from '@mdi/js'
+	import { slide } from 'svelte/transition'
+	
+	import type { PageData } from './$types'
 	import { Icon, Pagination } from '$lib/material'
 	import { urlParam } from '$lib/store'
-	import { slide } from 'svelte/transition'
 	import Members from './Members.svelte'
 	import MembersCopy from './MembersCopy.svelte'
 	import MembersFilter from './MembersFilter.svelte'
 	import MembersStats from './MembersStats.svelte'
-	import { mdiSigma } from '@mdi/js'
+	import ColumnsSelect, { type Column } from '$lib/ColumnsSelect.svelte'
+	import { getAge } from '$lib/utils'
+	import { jsonParse } from '$lib/jsonParse'
 
 	export let data
 
@@ -22,13 +27,41 @@
 		{}
 	)
 
-	let selectedColumnsId: string[]
+	const defaultColumnsId = ['periods', 'hours', 'sectors']
+	let selectedColumnsId = defaultColumnsId
+
+	type Member = PageData['members'][number]
+	const toHour = (ms: number) => ms / (1000 * 60 * 60)
+	const columns: Record<string, Column<Member>> = {
+		periods: { label: 'Périodes', getValue: (m) => m.subscribes.length },
+		hours: { label: 'Heures', getValue: (m) => toHour(workTimes[m.id]) },
+		sectors: { label: 'Secteurs à charges', getValue: (m) => m.leaderOf.map(({ name }) => name) },
+		age: { label: 'Age', getValue: (m) => getAge(m.user.birthday) },
+		...data.fields.reduce(
+			(acc, cur) => ({
+				...acc,
+				[cur.id]: {
+					label: cur.name,
+					getValue: (m: Member) => {
+						const { value } = m.profile.find((f) => f.fieldId === cur.id) || { value: '' }
+						if (!value) return ''
+						if (cur.type === 'multiselect') return jsonParse(value, [])
+						if (cur.type === 'boolean') return value === 'true'
+						if (cur.type === 'number') return +value
+						return value
+					},
+				},
+			}),
+			{}
+		),
+	}
 </script>
 
 <div class="flex flex-col gap-4">
 	<div class="flex gap-x-3 gap-y-2 flex-wrap">
 		<MembersFilter fields={data.fields} teams={data.teams} />
 		<MembersCopy members={data.members} fields={data.fields} {selectedColumnsId} />
+		<ColumnsSelect {columns} {defaultColumnsId} bind:selectedColumnsId />
 
 		<a
 			class="
@@ -54,7 +87,7 @@
 		</div>
 	{/if}
 
-	<Members members={data.members} fields={data.fields} {workTimes} bind:selectedColumnsId />
+	<Members members={data.members} {columns} bind:selectedColumnsId />
 
 	<div class="flex justify-end">
 		<Pagination />
--- src/routes/[eventId]/admin/manage/members/Members.svelte
+++ src/routes/[eventId]/admin/manage/members/Members.svelte
@@ -1,117 +1,79 @@
 <script lang="ts">
 	import { goto } from '$app/navigation'
-
-	import { getAge } from '$lib/utils'
 	import { eventPath } from '$lib/store'
 	import { Placeholder } from '$lib/material'
 	import Contact from '$lib/Contact.svelte'
-	import { jsonParse } from '$lib/jsonParse'
 
 	import type { PageData } from './$types'
-	import ColumnsSelect, { type Column } from '$lib/ColumnsSelect.svelte'
+	import type { Column } from '$lib/ColumnsSelect.svelte'
 
 	type Member = PageData['members'][number]
 	export let members: Member[]
-	export let fields: PageData['fields']
-	export let workTimes: Record<string, number> = {}
-
-	const toHour = (ms: number) => ms / (1000 * 60 * 60)
-
-	const columns: Record<string, Column<Member>> = {
-		periods: { label: 'Périodes', getValue: (m) => m.subscribes.length },
-		hours: { label: 'Heures', getValue: (m) => toHour(workTimes[m.id]) },
-		sectors: { label: 'Secteurs à charges', getValue: (m) => m.leaderOf.map(({ name }) => name) },
-		age: { label: 'Age', getValue: (m) => getAge(m.user.birthday) },
-		...fields.reduce(
-			(acc, cur) => ({
-				...acc,
-				[cur.id]: {
-					label: cur.name,
-					getValue: (m: Member) => {
-						const { value } = m.profile.find((f) => f.fieldId === cur.id) || { value: '' }
-						if (!value) return ''
-						if (cur.type === 'multiselect') return jsonParse(value, [])
-						if (cur.type === 'boolean') return value === 'true'
-						if (cur.type === 'number') return +value
-						return value
-					},
-				},
-			}),
-			{}
-		),
-	}
 
 	const defaultColumnsId = ['periods', 'hours', 'sectors']
 	export let selectedColumnsId = defaultColumnsId
+	export let columns: Record<string, Column<Member>>
 
 	let limit = 50
 	const step = 50
 </script>
 
-<div class="contents">
-	<div class="relative z-10">
-		<div class="absolute right-8">
-			<ColumnsSelect {columns} {defaultColumnsId} bind:selectedColumnsId />
-		</div>
-	</div>
-
-	<div class="table-wrapper">
-		{#if members.length}
-			<table class="table table-pin-rows">
-				<thead>
-					<tr class="shadow">
-						<th class="sticky left-0 bg-base-100">Nom</th>
-						{#each selectedColumnsId as colId}
-							<th>{columns[colId].label}</th>
-						{/each}
-						<th />
-					</tr>
-				</thead>
-
-				<tbody>
-					{#each members.slice(0, limit) as member (member.id)}
-						<tr
-							on:click={() => goto(`${$eventPath}/admin/manage/members/${member.id}`)}
-							class="hover cursor-pointer group"
-						>
-							<td class="sticky left-0 bg-base-100 group-hover:bg-base-200 border-r">
-								{member.user.firstName}
-								{member.user.lastName}
-							</td>
-
-							{#each selectedColumnsId as colId}
-								{@const value = columns[colId].getValue(member)}
-								<td>
-									{#if Array.isArray(value)}
-										{#each value as v}
-											<span class="badge badge-sm mr-1 whitespace-nowrap">
-												{v}
-											</span>
-										{/each}
-									{:else if typeof value === 'number'}
-										<span class="badge">{value}</span>
-									{:else if typeof value === 'boolean'}
-										<span class="badge">{value ? 'OUI' : 'NON'}</span>
-									{:else}
-										<span>{value}</span>
-									{/if}
-								</td>
-							{/each}
-
-							<td align="right">
-								<Contact user={{ ...member.user }} />
-							</td>
-						</tr>
+<div class="table-wrapper">
+	{#if members.length}
+		<table class="table table-pin-rows">
+			<thead>
+				<tr class="shadow">
+					<th class="sticky left-0 bg-base-100">Membre</th>
+					{#each selectedColumnsId as colId}
+						<th>{columns[colId].label}</th>
 					{/each}
-				</tbody>
-			</table>
-			{#if limit < members.length}
-				<div class="w-full grid place-content-center sticky left-0 p-6">
-					<button class="btn btn-sm" on:click={() => (limit += step)}>Afficher plus</button>
-				</div>
-			{/if}
-		{:else}
-			<Placeholder>Aucun membre trouvé</Placeholder>
+					<th />
+				</tr>
+			</thead>
+
+			<tbody>
+				{#each members.slice(0, limit) as member (member.id)}
+					<tr
+						on:click={() => goto(`${$eventPath}/admin/manage/members/${member.id}`)}
+						class="hover cursor-pointer group"
+					>
+						<td>
+							{member.user.firstName}
+							{member.user.lastName}
+						</td>
+
+						{#each selectedColumnsId as colId}
+							{@const value = columns[colId].getValue(member)}
+							<td>
+								{#if Array.isArray(value)}
+									{#each value as v}
+										<span class="badge badge-sm mr-1 whitespace-nowrap">
+											{v}
+										</span>
+									{/each}
+								{:else if typeof value === 'number'}
+									<span class="badge">{value}</span>
+								{:else if typeof value === 'boolean'}
+									<span class="badge">{value ? 'OUI' : 'NON'}</span>
+								{:else}
+									<span>{value}</span>
+								{/if}
+							</td>
+						{/each}
+
+						<td align="right">
+							<Contact user={{ ...member.user }} />
+						</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+		{#if limit < members.length}
+			<div class="w-full grid place-content-center sticky left-0 p-6">
+				<button class="btn btn-sm" on:click={() => (limit += step)}>Afficher plus</button>
+			</div>
 		{/if}
-	</div>
+	{:else}
+		<Placeholder>Aucun membre trouvé</Placeholder>
+	{/if}
 </div>
