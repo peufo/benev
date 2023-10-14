27b75683fea5138d3bffcdad2c17fb076d68c52a
jonas <jonas.voisard@gmail.com>
2023-10-14 11:02:36 +0200

    use icon for select memberType

--- src/routes/[eventId]/admin/manage/members/MembersFilter.svelte
+++ src/routes/[eventId]/admin/manage/members/MembersFilter.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import { Field } from '@prisma/client'
+	import { mdiAccountCircleOutline, mdiShieldAccount } from '@mdi/js'
+
 	import PeriodPickerButton from '$lib/period/PeriodPickerButton.svelte'
 	import { InputCheckboxsMenu, InputOptionInParam } from '$lib/material'
 	import FieldsFilter from '$lib/FieldsFilter.svelte'
@@ -21,8 +23,8 @@
 <InputOptionInParam
 	key="memberType"
 	options={{
-		volunteers: 'Bénévoles',
-		leaders: 'Responsables',
+		leaders: { label: 'Responsables (au moins un secteur à charge)', icon: mdiShieldAccount },
+		volunteers: { label: 'Bénévoles (au moins une inscription)', icon: mdiAccountCircleOutline },
 	}}
 />
 
