a40d2689a82c4159f4e0e9122edbbbd50edf03af
jonas <jonas.voisard@gmail.com>
2023-10-17 12:40:24 +0200

    use the same button for better focus managment

--- src/lib/material/DeleteButton.svelte
+++ src/lib/material/DeleteButton.svelte
@@ -6,7 +6,9 @@
 
 	let ready = false
 	let timeout: NodeJS.Timeout
-	function handleClick() {
+	function handleClick(event: MouseEvent) {
+		if (ready) return
+		event.preventDefault()
 		ready = true
 		timeout = setTimeout(() => (ready = false), 2000)
 	}
@@ -17,16 +19,17 @@
 	}
 </script>
 
-{#if !ready}
-	<button
-		class="btn text-error btn-ghost w-28 {klass}"
-		class:btn-disabled={disabled}
-		on:click={handleClick}
-	>
-		Supprimer
-	</button>
-{:else}
-	<button class="btn btn-error btn-outline w-28" {formaction} on:mouseleave={handleLeave}>
-		T'es sur ?
-	</button>
-{/if}
+<button
+	class="
+			btn w-28
+			{ready ? 'btn-error btn-outline w-28' : 'text-error btn-ghost'}
+			text-error btn-ghost
+			{klass}
+		"
+	class:btn-disabled={disabled}
+	on:click={handleClick}
+	on:mouseleave={handleLeave}
+	formaction={ready ? formaction : undefined}
+>
+	{ready ? "T'es sur ?" : 'Supprimer'}
+</button>
