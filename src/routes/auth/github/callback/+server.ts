b61f1f4169572638b16352caccf201d8d08bdf10
jonas <jonas.voisard@gmail.com>
2023-11-09 17:39:56 +0100

    use getExistingUser provided by Lucia

--- src/routes/auth/github/callback/+server.ts
+++ src/routes/auth/github/callback/+server.ts
@@ -11,16 +11,15 @@ export const GET = async ({ url, cookies, locals }) => {
 	if (storedState !== state) throw error(400)
 
 	try {
-		const { githubUser, createUser } = await githubAuth.validateCallback(code)
-
-		const getExistingUser = async () => {
-			if (!githubUser.email) return null
-			return prisma.user.findFirst({ where: { email: githubUser.email } })
-		}
+		const { getExistingUser, githubUser, createUser } = await githubAuth.validateCallback(code)
 
 		const getUser = async () => {
 			const existingUser = await getExistingUser()
 			if (existingUser) return existingUser
+			if (githubUser.email) {
+				const dbUser = await prisma.user.findFirst({ where: { email: githubUser.email } })
+				if (dbUser) return { ...dbUser, userId: dbUser.id }
+			}
 
 			const email = githubUser.email || `${githubUser.login}@benev.io`
 			const firstName = githubUser.name?.split(' ')[0] || githubUser.login
@@ -45,7 +44,7 @@ export const GET = async ({ url, cookies, locals }) => {
 		locals.auth.setSession(session)
 		return new Response(null, {
 			status: 302,
-			headers: { Location: '/' },
+			headers: { Location: '/me' },
 		})
 	} catch (e) {
 		if (e instanceof OAuthRequestError) {
