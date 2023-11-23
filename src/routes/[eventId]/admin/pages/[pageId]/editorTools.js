ac4a74ad2c8cded6359c8277edda7897218de902
jonas <jonas.voisard@gmail.com>
2023-11-23 13:58:42 +0100

    add marker

--- src/routes/[eventId]/admin/pages/[pageId]/editorTools.js
+++ src/routes/[eventId]/admin/pages/[pageId]/editorTools.js
@@ -2,8 +2,11 @@
 import Header from '@editorjs/header'
 // @ts-ignore
 import List from '@editorjs/list'
+// @ts-ignore
+import Marker from '@editorjs/marker'
 
 export const tools = {
 	header: Header,
 	list: List,
+	marker: Marker,
 }
