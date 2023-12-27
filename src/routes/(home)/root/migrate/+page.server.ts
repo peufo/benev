676305642fc70b6f2f64d89e4d50a43315f70bc6
jonas <jonas.voisard@gmail.com>
2023-12-27 19:24:56 +0100

    remove editorjs

--- src/routes/(home)/root/migrate/+page.server.ts
+++ src/routes/(home)/root/migrate/+page.server.ts
@@ -1,7 +1,4 @@
 import { prisma, tryOrFail } from '$lib/server'
-import { tiptap } from '$lib/pages'
-import EditorJsHTML from 'editorjs-html'
-import { jsonParse } from '$lib/jsonParse.js'
 
 export const actions = {
 	generate_all_avatars: async () => {
@@ -17,29 +14,5 @@ export const actions = {
 			)
 			return
 		})
-	},
-	editorjs_to_tiptap: async () => {
-		const parser = EditorJsHTML()
-
-		const pages = await prisma.page.findMany()
-
-		pages.forEach(async ({ id, content, title }) => {
-			try {
-				const json = jsonParse<any>(content, { blocks: [] })
-				const blocks = json !== null ? parser.parse(json) : []
-				const html = blocks.join()
-
-				const newContent = tiptap.toJSON(html)
-
-				await prisma.page.update({
-					where: { id },
-					data: { content: newContent },
-				})
-			} catch (error) {
-				console.error(error)
-			}
-		})
-
-		return
 	},
 }
