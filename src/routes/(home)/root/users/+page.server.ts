928e357347045e44b2ec71ad5e84517a738c48db
jonas <jonas.voisard@gmail.com>
2024-01-07 21:02:18 +0100

    feat: add users tables

---
+++ src/routes/(home)/root/users/+page.server.ts
@@ -0,0 +1,22 @@
+import { z } from '$lib/validation'
+import { parseQuery, prisma } from '$lib/server'
+import { error } from '@sveltejs/kit'
+
+export const load = async ({ url }) => {
+	const { data, err } = parseQuery(url, {
+		take: z.number().default(20),
+		skip: z.number().default(0),
+	})
+	if (err) throw error(400)
+	return {
+		usersCount: await prisma.user.count(),
+		users: await prisma.user.findMany({
+			take: data.take,
+			skip: data.skip,
+			include: {
+				_count: { select: { members: true, events: true } },
+			},
+			orderBy: { createdAt: 'desc' },
+		}),
+	}
+}
---
+++ src/routes/(home)/root/users/+page.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import { Card } from '$lib/material'
+
+	export let data
+</script>
+
+<Card>
+	<h2 class="title">Users ({data.usersCount})</h2>
+
+	<table class="table">
+		<thead>
+			<tr>
+				<td>Name</td>
+				<td>Email</td>
+				<td>CreatedAt</td>
+				<td>Events</td>
+				<td>Members</td>
+			</tr>
+		</thead>
+		<tbody>
+			{#each data.users as user}
+				<tr>
+					<td>
+						<a href="/root/users/{user.id}" class="link link-hover">
+							{user.firstName}
+							{user.lastName}
+						</a>
+					</td>
+					<td>
+						<a href="mailto:{user.email}">
+							{user.email}
+						</a>
+					</td>
+					<td>
+						{user.createdAt.toLocaleDateString()}
+					</td>
+					<td>{user._count.events}</td>
+					<td>{user._count.members}</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</Card>
