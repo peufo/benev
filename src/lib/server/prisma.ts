d5ac38d70a01717b8ba771761b12991462df186f
jonas <jonas.voisard@gmail.com>
2024-01-09 21:55:55 +0100

    fix: add unique key when remove Event

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,11 +1,12 @@
 import { PrismaClient } from '@prisma/client'
+import { createId } from '@paralleldrive/cuid2'
 
 export const prisma = new PrismaClient().$extends({
 	query: {
 		event: {
 			async delete({ args: { where } }) {
 				const event = await prisma.event.findUniqueOrThrow({ where })
-				const id = `deleted_${event.id}`
+				const id = `deleted_${event.id}_${createId()}`
 				await prisma.event.update({ where, data: { id, deletedAt: new Date() } })
 				return true
 			},
