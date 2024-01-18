b85ca51d07497be418007654f6c4db380e436bd5
jonas <jonas.voisard@gmail.com>
2024-01-18 21:50:18 +0100

    feat: add "View" in DB

---
+++ prisma/migrations/20240118204956_/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE `View` (
+    `id` VARCHAR(191) NOT NULL,
+    `key` VARCHAR(191) NOT NULL,
+    `query` TEXT NOT NULL,
+    `eventId` VARCHAR(191) NOT NULL,
+    `authorId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `View` ADD CONSTRAINT `View_eventId_fkey` FOREIGN KEY (`eventId`) REFERENCES `Event`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `View` ADD CONSTRAINT `View_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `Member`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
