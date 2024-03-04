f0c930a4e3c6737e22f3e5f8ee661a1aad420044
jonas <jonas.voisard@gmail.com>
2024-03-04 19:10:56 +0100

    test: write basique test for create user and event

--- tests/test.ts
+++ tests/test.ts
@@ -1,4 +1,5 @@
-import { expect, test } from '@playwright/test'
+import { expect, test, type Page } from '@playwright/test'
+import cuid from '@paralleldrive/cuid2'
 
 test('Open landing page', async ({ page }) => {
 	await page.goto('/')
@@ -6,26 +7,53 @@ test('Open landing page', async ({ page }) => {
 	await expect(page).toHaveTitle(/Benev\.io/)
 })
 
-test('User register', async ({ page }) => {
-	await page.goto('http://localhost:5173/')
-	await page.getByRole('link', { name: 'Essayer maintenant' }).click()
-	await page.getByRole('button', { name: 'Nouveau compte' }).click()
-	await page.getByLabel('Prénom').click()
-	await page.getByLabel('Prénom').fill('Bob')
-	await page.getByLabel('Prénom').press('Tab')
-	await page.getByLabel('Nom', { exact: true }).fill('Tester')
-	await page.getByLabel('Nom', { exact: true }).press('Tab')
-	await page.getByLabel('Email').fill('bob.dada@benev.io')
-	await page.getByLabel('Email').press('Tab')
-	await page.getByLabel('Mot de passe').fill('12341234')
-	await page.getByLabel('Nom', { exact: true }).dblclick()
-	await page.getByLabel("J'accepte\n\t\t\t\t\t\tles conditions d'utilisation").check()
-	await page.getByRole('button', { name: 'Valider' }).click()
-	await expect(page.locator('main')).toHaveText('Mes évènements')
-})
+test.describe('New user and new event', () => {
+	const testerId = cuid.createId()
+	const randomEventId = cuid.createId()
+	const eventName = `Test event ${randomEventId}`
 
-test('Create an event', async ({ page }) => {
-	await page.getByRole('button', { name: 'Organiser' }).click()
-	await page.getByRole('button', { name: 'Oui, je le veux' }).click()
-	await page.getByLabel("Nom de l'évènement").fill('Skate contest')
+	test.beforeEach(async ({ page }) => login(page))
+
+	const userIsLogged = async (page: Page) => {
+		return expect(page.getByRole('heading', { name: 'Mes évènements' }))
+			.toBeVisible()
+			.then(() => true)
+			.catch(() => false)
+	}
+
+	async function login(page: Page) {
+		if (await userIsLogged(page)) return
+		await page.goto('/me')
+		await page.getByLabel('Email').fill(`bob-${testerId}@benev.io`)
+		await page.getByLabel('Mot de passe').fill('12341234')
+		await page.locator('button').filter({ hasText: 'Connexion' }).click()
+
+		if (await userIsLogged(page)) return
+		await page.getByRole('button', { name: 'Nouveau compte' }).click()
+		await page.getByLabel('Prénom').click()
+		await page.getByLabel('Prénom').fill('Bob')
+		await page.getByLabel('Prénom').press('Tab')
+		await page.getByLabel('Nom', { exact: true }).fill(testerId)
+		await page.getByLabel('Nom', { exact: true }).press('Tab')
+		await page.getByLabel('Email').fill(`bob-${testerId}@benev.io`)
+		await page.getByLabel('Email').press('Tab')
+		await page.getByLabel('Mot de passe').fill('12341234')
+		await page.getByLabel('Nom', { exact: true }).dblclick()
+		await page.getByLabel("J'accepte\n\t\t\t\t\t\tles conditions d'utilisation").check()
+		await page.getByRole('button', { name: 'Valider' }).click()
+	}
+
+	test('User login', async ({ page }) => {
+		await expect(page.getByRole('heading', { name: 'Mes évènements' })).toBeVisible()
+	})
+
+	test('Create an event', async ({ page }) => {
+		await page.goto('/me')
+		await page.getByRole('button', { name: 'Organiser' }).click()
+		await page.getByRole('button', { name: 'Oui, je le veux' }).click()
+		await page.getByLabel("Nom de l'évènement").fill(eventName)
+		await expect(page.getByLabel("URL de l'évènement")).toHaveValue(`test-event-${randomEventId}`)
+		await page.locator('button[type="submit"]').click()
+		await expect(page).toHaveTitle(`Benev.io - ${eventName}`)
+	})
 })
