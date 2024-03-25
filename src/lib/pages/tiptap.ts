20a77704a46b919aa46048f57699ae3185969aeb
jonas <jonas.voisard@gmail.com>
2024-03-26 00:22:00 +0100

    chore: add good type JSONContent

--- src/lib/pages/tiptap.ts
+++ src/lib/pages/tiptap.ts
@@ -1,11 +1,12 @@
 import { generateHTML, generateJSON } from '@tiptap/html'
+import type { JSONContent } from '@tiptap/core'
 import { extensions } from '$lib/material/input/textRich/extensions'
 import { jsonParse } from '$lib/jsonParse'
 
 export const tiptap = {
 	toHTML(jsonAsString: string) {
 		try {
-			const json = jsonParse(jsonAsString, [])
+			const json = jsonParse<JSONContent>(jsonAsString, [])
 			if (!json) return ''
 			return generateHTML(json, extensions)
 		} catch (err) {
