e815645cb934a7907bb55d6f285d4d13f651e0b7
jonas <jonas.voisard@gmail.com>
2024-05-30 16:52:43 +0200

    remove periodPicker

--- src/lib/period/index.ts
+++
@@ -1,4 +0,0 @@
-export type Period = { start: string; end: string }
-
-export { default as PeriodPicker } from './PeriodPicker.svelte'
-export { default as PeriodPickerButton } from './PeriodPickerButton.svelte'
