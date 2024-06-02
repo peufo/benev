cf607b0b020e157903f5c76f81f5221b00ac9e34
jonas <jonas.voisard@gmail.com>
2024-06-02 15:20:39 +0200

    style: remove unwanted padding

--- src/lib/period/PeriodDrawer.svelte
+++ src/lib/period/PeriodDrawer.svelte
@@ -38,7 +38,7 @@
 	{#if period?.id}
 		<div class="divider" />
 
-		<div class="p-2 flex flex-col gap-2">
+		<div class="flex flex-col gap-2">
 			{#if period.subscribes && period.maxSubscribe}
 				<Progress
 					period={{ maxSubscribe: period.maxSubscribe, subscribes: period.subscribes }}
