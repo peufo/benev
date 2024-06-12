f3e7a17b643911d8b6f4f247ff3e3efb7fcd750d
jonas <jonas.voisard@gmail.com>
2024-06-12 15:18:50 +0200

    test publish ical

---
+++ src/routes/api/ical/+server.ts
@@ -0,0 +1,29 @@
+import ical from 'ical-generator'
+
+export const GET = async ({ url }) => {
+	const token = url.searchParams.get('token')
+	console.log({ token })
+	// Create and use user token
+
+	const calendar = ical({ name: 'Mon bénévolat' })
+
+	const startTime = new Date()
+	const endTime = new Date()
+	endTime.setHours(startTime.getHours() + 1)
+
+	calendar.createEvent({
+		start: startTime,
+		end: endTime,
+		summary: 'Secteur XY',
+		description: 'It works ;)',
+		location: 'my room',
+		url: 'https://benev.io/me',
+	})
+
+	return new Response(calendar.toString(), {
+		headers: {
+			'Content-type': 'text/calendar; charset=utf-8',
+			'Content-Disposition': 'attachment; filename="calendar.ics"',
+		},
+	})
+}
