f83511d530bb4631a379a13aa63525b0f11cbf76
jonas <jonas.voisard@gmail.com>
2024-06-28 17:07:10 +0200

    feat: add filter by teams

--- src/routes/[eventId]/admin/plan/+page.svelte
+++ src/routes/[eventId]/admin/plan/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { jsonParse, RangePickerButton, urlParam, TabsIcon, Icon } from 'fuma'
+	import { jsonParse, RangePickerButton, urlParam, TabsIcon, Icon, InputCheckboxsMenu } from 'fuma'
 	import { mdiAlignHorizontalLeft, mdiAlignVerticalTop, mdiOpenInNew } from '@mdi/js'
 	import { page } from '$app/stores'
 	import { PlanV, PlanH, ZoomButton } from '$lib/plan'
@@ -19,9 +19,18 @@
 	<h2 class="title">Planification</h2>
 	<div class="grow" />
 
-	<PeriodCardOptions />
+	<InputCheckboxsMenu
+		key="teams"
+		options={data.teams.map((t) => ({ value: t.id, label: t.name }))}
+		enhanceDisabled
+		badgePrimary
+	>
+		<span slot="label" class="font-normal">secteurs</span>
+	</InputCheckboxsMenu>
 
 	<RangePickerButton />
+
+	<PeriodCardOptions />
 
 	<ZoomButton bind:value={hourSize} min={5} max={100} step={1} />
 
@@ -50,3 +59,8 @@
 		<PlanH teams={data.teams_periods} {range} {hourSize} />
 	{/if}
 </div>
+
+<style>
+	.prout {
+	}
+</style>
