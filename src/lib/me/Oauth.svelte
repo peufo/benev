31fce02627fce961f008545cf9cd986fdb66c870
jonas <jonas.voisard@gmail.com>
2025-06-11 12:47:28 +0200

    disable connection with github

--- src/lib/me/Oauth.svelte
+++ src/lib/me/Oauth.svelte
@@ -7,20 +7,24 @@
 <div class="flex p-4 gap-4 justify-center">
 	<a
 		href="/auth/google"
-		class="btn btn-square"
+		class="btn lowercase"
 		data-sveltekit-preload-data="off"
 		data-sveltekit-preload-code="off"
-		use:tip={{ content: 'Connexion avec Google' }}
 	>
 		<img src={googleLogo} alt="Logo Google" />
+		<span>Connexion avec Google</span>
 	</a>
+	<!-- 
+		TODO: ensure user share his email 
+	 	https://github.com/peufo/benev/issues/136
 
-	<a
-		href="/auth/github"
-		class="btn btn-square"
-		data-sveltekit-preload-data="off"
-		data-sveltekit-preload-code="off"
-	>
-		<Icon path={mdiGithub} title="Connexion avec Github" />
-	</a>
+		<a
+			href="/auth/github"
+			class="btn btn-square"
+			data-sveltekit-preload-data="off"
+			data-sveltekit-preload-code="off"
+		>
+			<Icon path={mdiGithub} title="Connexion avec Github" />
+		</a>
+	-->
 </div>
