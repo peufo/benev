27195d367955c3f6b1ad6e42bca27e0fcae6c199
jonas <jonas.voisard@gmail.com>
2025-09-27 11:01:41 +0200

    fix: Home menu is not relatif

--- src/routes/(home)/HomeMenuItems.svelte
+++ src/routes/(home)/HomeMenuItems.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { mdiCalendarMultiple, mdiForumOutline, mdiScaleBalance, mdiWalletOutline } from '@mdi/js'
 	import { page } from '$app/stores'
-	import { eventPath } from '$lib/store'
 	import { Icon } from 'fuma'
 
 	const tabs: { path: string; label: string; icon: string }[] = [
@@ -14,7 +13,7 @@
 
 {#each tabs as { path, label, icon }}
 	{@const active = $page.route.id?.startsWith(`/(home)${path}`)}
-	<a href="{$eventPath}{path}" class="menu-item" class:active>
+	<a href={path} class="menu-item" class:active>
 		<Icon path={icon} size={20} class="opacity-70" />
 		{label}
 	</a>
