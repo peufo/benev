4caecfc33f87cf773d522b177d8e94ac355fac71
jonas <jonas.voisard@gmail.com>
2026-02-26 10:22:29 +0100

    Responsable badge are clickable

--- src/lib/team/InputLeaders.svelte
+++ src/lib/team/InputLeaders.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import type { Member } from '@prisma/client'
 	import { mdiAccountPlusOutline } from '@mdi/js'
-	import { InputRelations, urlParam } from 'fuma'
+	import { component, InputRelations, urlParam } from 'fuma'
 	import { api } from '$lib/api'
+	import MemberLink from './MemberLink.svelte'
 
 	export let value: Member[] | undefined = undefined
 </script>
@@ -12,7 +13,7 @@
 	label="Responsables"
 	{value}
 	search={$api.member.search}
-	slotItem={({ firstName, lastName }) => `${firstName} ${lastName}`}
+	slotItem={({ id, firstName, lastName }) => component(MemberLink, { id, firstName, lastName })}
 	createTitle="Inviter un nouveau membre"
 	createIcon={mdiAccountPlusOutline}
 	createUrl={$urlParam.with({ form_invite: '{}' })}
---
+++ src/lib/team/MemberLink.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { eventPath } from '$lib/store'
+
+	export let id: string
+	export let firstName: string
+	export let lastName: string
+</script>
+
+<a href="{$eventPath}/admin/members/{id}" class="link link-hover">
+	{firstName}
+	{lastName}
+</a>
