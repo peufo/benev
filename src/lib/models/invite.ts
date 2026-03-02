32ae5a34156ad93181ba4f610f181207557037d0
jonas <jonas.voisard@gmail.com>
2026-03-02 20:17:43 +0100

    fix: member empty email need to be "null" to respect the unique constrainte

--- src/lib/models/invite.ts
+++ src/lib/models/invite.ts
@@ -1,7 +1,7 @@
 import { z } from 'fuma'
 
 export const modelInvite = {
-	email: z.union([z.string().email().toLowerCase(), z.string().max(0)]).nullish(),
+	email: z.union([z.string().email().toLowerCase(), z.literal("").transform(() => null)]).nullish(),
 	firstName: z.string().min(2),
 	lastName: z.string().min(2),
 }
