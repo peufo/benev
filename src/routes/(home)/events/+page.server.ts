bd804ac91beeec2711a51b2bca24ae45c7854aae
jonas <jonas.voisard@gmail.com>
2026-03-06 11:53:16 +0100

    fix: sort events

--- src/routes/(home)/events/+page.server.ts
+++ src/routes/(home)/events/+page.server.ts
@@ -1,6 +1,9 @@
 import { prisma } from '$lib/server'
 
 export const load = async () => {
-	const events = await prisma.event.findMany({ where: { state: 'published', deletedAt: null } })
+	const events = await prisma.event.findMany({
+		where: { state: 'published', deletedAt: null },
+		orderBy: [{ startDate: 'desc' }, { createdAt: 'desc' }],
+	})
 	return { events }
 }
