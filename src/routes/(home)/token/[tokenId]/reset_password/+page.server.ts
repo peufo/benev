51f65a12948e097720c91dc4640c4c9c0bf1ef8a
jonas <jonas.voisard@gmail.com>
2026-05-07 15:27:53 +0200

    fix: password reset pass user.id instead of user.email

--- src/routes/(home)/token/[tokenId]/reset_password/+page.server.ts
+++ src/routes/(home)/token/[tokenId]/reset_password/+page.server.ts
@@ -1,21 +1,20 @@
-import { parseFormData } from 'fuma/server'
+import { formAction } from 'fuma/server'
 import { z } from 'fuma/validation'
 import { validateToken } from '$lib/server'
 import { auth } from '$lib/server'
-import { error, fail, redirect } from '@sveltejs/kit'
 
 export const actions = {
-	default: async ({ params, locals, request }) => {
-		const { data } = await parseFormData(request, {
+	default: formAction(
+		{
 			password: z.string().min(8),
 			redirectTo: z.string().optional(),
-		}).catch((err) => fail(400, err.issues))
-
-		try {
+		},
+		async ({ data, locals, params }) => {
 			const userId = await validateToken('passwordReset', params.tokenId)
 			const user = await auth.getUser(userId)
+
 			await auth.invalidateAllUserSessions(user.id)
-			await auth.deleteKey('email', user.id)
+			await auth.deleteKey('email', user.email)
 			await auth.createKey({
 				userId: user.id,
 				providerId: 'email',
@@ -30,11 +29,8 @@ export const actions = {
 
 			const session = await auth.createSession({ userId, attributes: {} })
 			locals.auth.setSession(session)
-		} catch {
-			error(401, 'Invalid token')
-		} finally {
-			if (data.redirectTo) redirect(302, data.redirectTo)
-			redirect(302, '/me')
-		}
-	},
+			return data.redirectTo || '/me'
+		},
+		{ redirectTo: (url) => url }
+	),
 }
