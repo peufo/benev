9996fbd2d7201ef65274ecab680f31434e8edde4
Jonas <jonas.voisard@gmail.com>
2026-06-11 15:46:46 +0200

    Admin navigation take less place

--- src/lib/layout/AdminNavigation.svelte
+++ src/lib/layout/AdminNavigation.svelte
@@ -1,18 +1,22 @@
 <script lang="ts">
-	import { drawerOpen } from '$lib/layout/store'
 	import { adminTabs } from '$lib/layout/adminTabs'
 	import { Card, Icon } from 'fuma'
 </script>
 
-{#if $drawerOpen}
-	<Card class="sticky top-4 min-w-56 hidden lg:block mt-4 ml-4" bodyClass="sm:px-2 sm:py-2">
-		<div class="flex flex-col gap-1 text-sm">
-			{#each $adminTabs as { href, isActive, label, icon }}
-				<a {href} class="menu-item hover:bg-base-200/80" class:active={isActive}>
-					<Icon path={icon} size={20} class="opacity-70 py-1" />
+<Card
+	class="sticky top-4 hidden lg:block mt-4 ml-4 whitespace-nowrap shrink-0
+		group max-w-[60px] hover:max-w-[300px] transition-[max-width] duration-300 overflow-hidden
+	"
+	bodyClass="sm:px-2 sm:py-2"
+>
+	<div class="flex flex-col gap-1 text-sm">
+		{#each $adminTabs as { href, isActive, label, icon }, index}
+			<a {href} class="menu-item hover:bg-base-200/80 overflow-hidden" class:active={isActive}>
+				<Icon path={icon} size={20} class="opacity-70 py-1" />
+				<span class="opacity-0 group-hover:opacity-100 transition-opacity">
 					{label}
-				</a>
-			{/each}
-		</div>
-	</Card>
-{/if}
+				</span>
+			</a>
+		{/each}
+	</div>
+</Card>
