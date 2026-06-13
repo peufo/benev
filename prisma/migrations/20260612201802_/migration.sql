3b8cddeb7a9388ba55578de93c69e3825cf84a90
Jonas <jonas.voisard@gmail.com>
2026-06-13 22:42:24 +0200

    fix: an event can be associated to many products

--- prisma/migrations/20260612201802_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `priceId` to the `Product` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `Product` ADD COLUMN `priceId` VARCHAR(191) NOT NULL;
--- prisma/migrations/20260613203618_add_products/migration.sql
+++ prisma/migrations/20260613203618_add_products/migration.sql
@@ -5,12 +5,12 @@ ALTER TABLE `Event` ADD COLUMN `tier` ENUM('basic', 'standard', 'premium', 'pro'
 CREATE TABLE `Product` (
     `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
+    `priceId` VARCHAR(191) NOT NULL,
     `eventId` VARCHAR(191) NULL,
     `checkoutId` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
     UNIQUE INDEX `Product_id_key`(`id`),
-    UNIQUE INDEX `Product_eventId_key`(`eventId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
