c99d42091b0a83b9b54825717afb02c2100365fe
Jonas <jonas.voisard@gmail.com>
2026-07-30 11:41:14 +0200

    use snippet and btn class for top menu

--- src/routes/(home)/HomeMenu.svelte
+++ src/routes/(home)/HomeMenu.svelte
@@ -1,11 +1,29 @@
 <script lang="ts">
-	import { Menu } from '@lucide/svelte'
+	import { CalendarDays, Heart, Menu, MessagesSquare, type IconProps } from '@lucide/svelte'
 	import { Popover } from 'fuma'
-	import HomeMenuItems from './HomeMenuItems.svelte'
+	import type { Component } from 'svelte'
+	import type { ClassValue } from 'svelte/elements'
+	import { page } from '$app/state'
+
+	const tabs: { path: string; label: string; Icon: Component<IconProps> }[] = [
+		{ path: '/open-source', label: 'Open source', Icon: Heart },
+		{ path: '/contact', label: 'Contact', Icon: MessagesSquare },
+		{ path: '/events', label: 'Évènements', Icon: CalendarDays },
+	]
 </script>
 
+{#snippet menuItems(klass: ClassValue, classActive: ClassValue)}
+	{#each tabs as { path, label, Icon } (path)}
+		{@const isActive = page.route.id?.startsWith(`/(home)${path}`)}
+		<a href={path} class={[klass, isActive && classActive]}>
+			<Icon size={20} class="opacity-70" />
+			{label}
+		</a>
+	{/each}
+{/snippet}
+
 <div class="gap-2 hidden lg:flex">
-	<HomeMenuItems />
+	{@render menuItems('btn btn-ghost', 'btn-active')}
 </div>
 
 <Popover class="my-1 min-w-50">
@@ -16,6 +34,6 @@
 	{/snippet}
 
 	<div class="menu w-full p-1 gap-1">
-		<HomeMenuItems />
+		{@render menuItems('menu-item', 'active')}
 	</div>
 </Popover>
