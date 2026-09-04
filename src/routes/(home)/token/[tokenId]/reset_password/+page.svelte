664c1c01917d4a58cccf33ade43a5da955e3ef3c
Jonas <jonas.voisard@gmail.com>
2026-09-04 09:05:20 +0200

    Use new style for password reset form

--- src/routes/(home)/token/[tokenId]/reset_password/+page.svelte
+++ src/routes/(home)/token/[tokenId]/reset_password/+page.svelte
@@ -1,35 +1,21 @@
 <script lang="ts">
 	import { page } from '$app/state'
 	import { InputString } from 'fuma'
+	import { LockKeyholeOpenIcon } from '@lucide/svelte'
 
-	import { Oauth } from '$lib/me'
+	import { Section } from '$lib/ui'
 	import { resetPassword } from './reset_password.remote'
 
 	const redirectTo = page.url.searchParams.get('redirectTo')
-	const newUser = page.url.searchParams.get('newUser')
-	const eventName = page.url.searchParams.get('eventName')
 </script>
 
-<div class="card bg-base-100 max-w-md m-auto shadow-lg">
-	<form {...resetPassword} class="card-body">
-		{#if newUser}
-			<h2 class="title mb-4">Salut {newUser}👋</h2>
-			{#if eventName}
-				<p>
-					Merci d'avoir accepté de rejoindre l'équipe bénévole
-					<b>{eventName}</b> ❤️
-				</p>
-				<br />
-			{/if}
-
-			<p>Pour accéder à ton compte, tu peux utiliser un des ces services :</p>
-			<Oauth />
-			<div class="divider"></div>
-			<p>Ou alors, tu peux définir ton mot de passe</p>
-		{:else}
-			<h2 class="title mb-4">Réinitialisation de ton mot de passe</h2>
-		{/if}
-
+<Section
+	id="password-reset"
+	title="Réinitialisation de ton mot de passe"
+	icon={LockKeyholeOpenIcon}
+	class="max-w-md mx-auto"
+>
+	<form {...resetPassword} class="space-y-3">
 		<InputString
 			field={resetPassword.fields.password}
 			type="password"
@@ -41,7 +27,7 @@
 			<input type="hidden" name="redirectTo" value={redirectTo} />
 		{/if}
 		<div class="flex justify-end">
-			<button class="btn">Valider</button>
+			<button class="btn btn-primary">Réinitialiser</button>
 		</div>
 	</form>
-</div>
+</Section>
